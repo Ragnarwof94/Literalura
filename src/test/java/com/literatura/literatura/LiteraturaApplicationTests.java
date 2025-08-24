15045f8e51aff1f6870dc5773416662fb3267eb1
Ragnarwof94 <rodolfo.guerrero.cisterna@gmail.com>
2025-08-24 00:04:43 -0400

    feat: Integrar correccion de Maven, ajuste de test y otras mejoras

--- src/test/java/com/literatura/literatura/LiteraturaApplicationTests.java
+++ src/test/java/com/literatura/literatura/LiteraturaApplicationTests.java
@@ -1,13 +1,15 @@
-package com.literatura.literatura;
+package com.literatura.literatura; // Asegúrate de que este sea el paquete correcto
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class LiteraturaApplicationTests {
+@SpringBootTest // Esta anotación ahora buscará la clase principal LiterAluraApplication
+class LiterAluraApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        // Este test verifica si el contexto de Spring Boot se carga correctamente.
+        // Si el contexto no se carga, este test fallará y generará el error del surefire plugin.
+    }
 
 }
